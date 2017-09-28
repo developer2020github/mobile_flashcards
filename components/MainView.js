e3cacd5716d41035a628b101a462ba7a6021e5b3
developer2020 <dev276236@gmail.com>
2017-09-28 11:26:07 +0400

    feat: converted list of decks in the main view into react native flat list

--- components/MainView.js
+++ components/MainView.js
@@ -1,17 +1,23 @@
 //this is the main view of the application ; shows list of available decks and an option to 
 //add a new one
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
 import { Decks } from "../utils/Decks"
 import { listOfObjectsToArray } from "../utils/lib"
 import DeckItem from "./DeckItem"
 import AddNewDeck from "./AddNewDeck"
-
+//{listOfDecks.map((deck, index)=>{return <DeckItem deck={deck} key={index}/>})}
 
 export default class MainView extends React.Component {
   addNewDeckPress = () =>{
 
   }
+
+  renderDeck = ({item}) =>{
+    //return   <Text>{item.title}</Text>
+    return <DeckItem deck={item} />
+ }
+ 
   render() {
     let listOfDecks = listOfObjectsToArray(Decks)
     //
@@ -19,7 +25,12 @@ export default class MainView extends React.Component {
       <View style={styles.container}>
         <Text>Main view</Text>
         <AddNewDeck onPress={this.addNewDeckPress}/>
-        {listOfDecks.map((deck, index)=>{return <DeckItem deck={deck} key={index}/>})}
+
+        <FlatList
+        data={listOfDecks}
+        renderItem={this.renderDeck}
+        keyExtractor={item => item.title}
+         />
 
       </View>
     );
