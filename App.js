9a1ea6f63eff54d7af31a2730a2cc60eeeb97fa2
developer2020 <dev276236@gmail.com>
2017-10-04 07:24:35 +0400

    feat: added static deck view

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MainView from './components/MainView'
 import NewDeckView from "./components/NewDeckView"
-import { populateLocalStorage } from "./utils/Decks"
+import DeckView from "./components/DeckView"
+import { populateLocalStorage, newDeck } from "./utils/Decks"
 
 
 //const ShowMainView = true
@@ -17,9 +18,13 @@ export default class App extends React.Component {
       return (
         <MainView />
       )
-    }*/
+    }
     return (
       <NewDeckView />
+    )
+    */
+    return  (
+      <DeckView deck={newDeck} />
     )
   }
 }
--- components/DeckView.js
+++ components/DeckView.js
@@ -1,1 +1,58 @@
 //this component will display a particular deck 
+//this component will handle a view for addition of new deck. 
+import React, { Component } from 'react';
+import { AppRegistry, TextInput, Text, View , StyleSheet } from 'react-native';
+import CommonButton from "./CommonButton"
+
+export default class DeckView extends Component {
+ 
+
+  startQuizPress = ()=>{
+    console.log("Start new quiz pressed")
+  }
+
+  addNewCardPress = ()=>{
+    console.log("add new card pressed")
+  }
+
+  render() {
+    const numberOfCards = this.props.deck.questions.length
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header2}>{"Selected deck:"}</Text>
+        <Text style={styles.header}>{this.props.deck.title}</Text>
+        <Text style={styles.header}>{numberOfCards + " cards"}</Text>
+
+        <CommonButton onPress={this.addNewCardPress} text={"ADD NEW CARD"} btnBackgroundColor="#d0d9e8"/>
+        <CommonButton onPress={this.startQuizPress} text={"START QUIZ"} btnBackgroundColor="#a7ea70"/>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center', 
+    marginTop: 100
+  },
+
+  header: {
+    fontSize: 20, 
+    marginBottom: 20, 
+    fontWeight: "bold"
+  }, 
+
+  header2: {
+    fontSize: 14, 
+    marginBottom: 10, 
+    fontStyle: "italic", 
+    textAlign: "center"
+  }
+
+});
