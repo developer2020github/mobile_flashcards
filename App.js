c1b1ada324142b5ef98e3ce6d99c61a6442d0f84
developer2020 <dev276236@gmail.com>
2017-10-06 11:58:37 +0400

    added initial view via routing

--- App.js
+++ App.js
@@ -7,16 +7,47 @@ import QuizView from "./components/QuizView"
 import NewCardView from "./components/NewCardView"
 
 import { populateLocalStorage, newDeck } from "./utils/Decks"
+import { StackNavigator } from 'react-navigation'
 
 //const ShowMainView = true
 //keep this for testing purposes so that local storage is not empty
 populateLocalStorage()
 
+const MainStackNavigatorRoutesConfig = {
+    MainView: {
+        screen: MainView
+        
+    }, 
+
+    NewDeckView: {
+        screen: NewDeckView
+    }, 
+
+    DeckView: {
+        screen: DeckView
+    }, 
+
+    QuizView: {
+        screen: QuizView
+    }, 
+
+    NewCardView: {
+        screen: NewCardView
+    }
+}
+
+
+const MainStackNavigatorConfig = {
+    initialRouteName: "MainView"
+}
+
+const AppMainStackNavigator = StackNavigator(MainStackNavigatorRoutesConfig, MainStackNavigatorConfig)
+
 export default class App extends React.Component {
 
   render() {
     return(
-      <MainView />
+      <AppMainStackNavigator />
     )
   /*  if (ShowMainView) {
       return (
